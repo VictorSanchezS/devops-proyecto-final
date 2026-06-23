8d9596ccca81558b728dddf35924c50e76c2bfd9
Victor Sanchez <vitucho_ss@outlook.com>
2026-06-23 14:10:51 -0500

    Migrar imagen de Docker Hub a Azure Container Registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.ubicacion
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = "devopsvictor2026"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
 resource "azurerm_container_group" "api" {
   name                = "devops-proyecto-final"
   location            = azurerm_resource_group.rg.location
@@ -24,9 +32,15 @@ resource "azurerm_container_group" "api" {
   ip_address_type     = "Public"
   dns_name_label      = "devops-proyecto-victor"
 
+  image_registry_credential {
+    server   = azurerm_container_registry.acr.login_server
+    username = azurerm_container_registry.acr.admin_username
+    password = azurerm_container_registry.acr.admin_password
+  }
+
   container {
     name   = "api"
-    image  = "vitukuak/devops-proyecto-final:latest"
+    image  = "${azurerm_container_registry.acr.login_server}/devops-proyecto-final:latest"
     cpu    = "0.5"
     memory = "1.5"
 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "url_api" {
   value = "http://${azurerm_container_group.api.fqdn}:8000"
 }
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
